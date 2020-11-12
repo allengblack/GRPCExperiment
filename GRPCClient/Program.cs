7ff3ddb8468a42d73e1e7e07e753d3d4466946e2
Gbolahan Allen <allengblack@gmail.com>
2020-11-12 08:33:34 +0100

    feat: intial commit: add server and client

---
+++ GRPCClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
+using GRPCExperiment;
+using System;
+
+namespace GRPCClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Users.UsersClient(channel);
+
+            var res = client.GetUser(new UserRequest { Id = "dsafsdsfd" });
+            Console.WriteLine($"{res.User.Id} {res.User.Name}");
+
+            var res2 = client.GetUsers(new Empty());
+            Console.WriteLine($"{res2.Users[0].Id} {res2.Users[0].Name}");
+            Console.WriteLine($"{res2.Users[1].Id} {res2.Users[1].Name}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GRPCServer/Services/UserService.cs
@@ -0,0 +1,37 @@
+﻿using Google.Protobuf.Collections;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GRPCExperiment.Services
+{
+    public class UserService : GRPCExperiment.Users.UsersBase
+    {
+        public override Task<UserResponse> GetUser(UserRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new UserResponse { User = new User { Name = "Gbolahan Allen", Id = Guid.NewGuid().ToString() } });
+        }
+
+        public override Task<UsersResponse> GetUsers(Empty request, ServerCallContext context)
+        {
+            return Task.FromResult(new UsersResponse
+            {
+                Users = {
+                    new User
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Michael Ikechi"
+                    },
+                    new User
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = "Olanike Usman"
+                    }
+                }
+            });
+        }
+    }
+}
